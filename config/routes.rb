19a5807069fb9c52a48144b943eb2d1478ddc1fd
Nancy Chang <nancy.chang@berkeley.edu>
2018-04-23 18:52:25 -0700

    added Exercise model, controller, and views (unfinished)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  get "/exercises/show", to: "exercises#show"
+  post "/exercises/create", to: "exercises#create", as: "create_exercise"
 end
