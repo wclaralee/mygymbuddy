8ee2dc480d1f398a447af7c21d3c4b2c62cfa3c6
claralee <wclaralee@berkeley.edu>
2018-04-23 18:21:22 -0700

    target controller/views/etc

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,5 +1,11 @@
 class TargetsController < ApplicationController
+	def index
+		@targets = Target.all
+	end
+
 	def show
 		@target = Target.find(params[:id])
+		@exercise = Exercise.where(target_id: params[:id])
+
 	end
 end
